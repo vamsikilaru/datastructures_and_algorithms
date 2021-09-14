22586aabd0ed3c9026dc61ed03fd1da2d3afd403
Vamsi Kilaru <vamsi.techdeveloper@gmail.com>
2021-09-13 22:53:34 -0700

    commit

--- Arrays/moveZerosToFront.py
+++ Arrays/moveZerosToFront.py
@@ -4,18 +4,18 @@ def move_zeros_to_front(arr):
         return
     read = write = len(arr)-1
     while read >=0:
-        print(read)
         if arr[read] !=0:
             arr[write]=arr[read]
             read -=1
             write -=1
         else:
             read -=1
-    while write >=0:
+    while write >= 0:
         arr[write]=0
+        write -=1
 
 if __name__ == '__main__':
     arr = [10,11,0,-4,7,0,6,9,0,3]
-    print(arr)
+    print("before:",arr)
     move_zeros_to_front(arr)
-    print(arr)
+    print("after:",arr)
