73574d989130e36cef9b745e6b36040d03473db4
Vamsi Kilaru <vamsi.techdeveloper@gmail.com>
2021-11-25 22:06:20 -0800

    commit

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -12,7 +12,13 @@ class Graph:
 
     def add_edge(self,source,destination):
         if source < self.vertices and destination < self.vertices:
-            self.array[source].insert_at_head(destination)
+            self.array[source].insert_at_head(destination)  # this is a directed graph
+            # for undirected graph add edge in both the directions
+            # self.array[destination].insert_at_head(source)
+
+        # If we were to implement an Undirected Graph i.e (1,0) == (0,1)
+        # We would create an edge from destination towards source as well
+        # i.e self.list[destination].insertAtHead(source)
 
     def print_graph(self):
         print(">> Adjacency List of Directed graph <<")
---
+++ Graphs/graphCycle.py
@@ -0,0 +1,40 @@
+from graph import Graph
+
+
+def detect_cycle(g:Graph):
+    visited = [False] * g.vertices
+    rec_node_stack = [False] * g.vertices
+    for node in range(g.vertices):
+        if detect_cycle_rec(g,node,visited,rec_node_stack):
+            return True
+    return False
+
+def detect_cycle_rec(g:Graph,node, visited,rec_node_stack):
+    if rec_node_stack[node]:
+        return True
+    if visited[node]:
+        return False
+    visited[node]=True
+    rec_node_stack[node]=True
+    head_node = g.array[node].head_node
+    while head_node:
+        adjacent_node = head_node.value
+        if detect_cycle_rec(g,adjacent_node,visited,rec_node_stack):
+            return True
+        head_node = head_node.next
+    rec_node_stack[node]= False
+    return False
+
+if __name__ == "__main__" :
+    g1 = Graph(4)
+    g1.add_edge(0, 1)
+    g1.add_edge(1, 2)
+    g1.add_edge(1, 3)
+    g1.add_edge(3, 0)
+    
+    g2 = Graph(3)
+    g2.add_edge(0, 1)
+    g2.add_edge(1, 2)
+
+    print(detect_cycle(g1))
+    print(detect_cycle(g2))
